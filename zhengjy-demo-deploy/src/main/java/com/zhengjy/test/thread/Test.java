3337c055b6e77696ff57812ecb0395188ff2678e
zjy <zjy@duiba.com.cn>
2017-07-05 20:15:27 +0800

    e2

--- zhengjy-demo-deploy/src/main/java/com/zhengjy/test/thread/Test.java
+++ zhengjy-demo-deploy/src/main/java/com/zhengjy/test/thread/Test.java
@@ -1,30 +1,27 @@
 package com.zhengjy.test.thread;
 
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Test {
-	static Object o = new Object();
-	public static  void get(int i){
-		try {
-			synchronized(o){
-				Thread.sleep(1000);
-				System.out.println(Thread.currentThread().getName());
-				
-			}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-	
+
 	public static void main(String[] args) {
+		final ReentrantLock lock = new ReentrantLock(true);
+		lock.lock();
+		lock.lock();
+
+
+
 		new Thread(new Runnable() {
-			
 			@Override
 			public void run() {
-				for(int i = 0;i<10;i++){
-					Thread.currentThread().setName("Thread---"+i);
-					get(i);
-					
-				}
+				lock.lock();
+				lock.unlock();
 			}
 		}).start();
+
 	}
+
+
+
 }
